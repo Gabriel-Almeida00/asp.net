bc6232117563183bd7aad10e5973246aca463b65
gabriel <gaamdal557@gmail.com>
2023-03-02 17:16:01 -0300

    populando a tabela categorias

---
+++ Lanches/Migrations/20230302201001_PopularCategorias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lanches.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Categorias(CategoriaNome, Descricao) " +
+                "values('Normal', 'Lanche feito com ingredientes normais')");
+
+            migrationBuilder.Sql("insert into Categorias(CategoriaNome, Descricao) " +
+               "values('Natural', 'Lanche feito com ingredientes naturais')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Categorias");
+        }
+    }
+}
