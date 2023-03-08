b8d4d27780ffeba73fa2b5751c45f5a17ec79707
gabriel <gaamdal557@gmail.com>
2023-03-08 14:58:55 -0300

    carrinho de compras

---
+++ Lanches/Models/CarrinhoCompra.cs
@@ -0,0 +1,34 @@
+﻿using Lanches.Context;
+
+namespace Lanches.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public  string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider service)
+        {
+            ISession session =
+                service.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            var context = service.GetService<AppDbContext>();
+
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CarrinhoId" , carrinhoId);
+
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }
+    }
+}
